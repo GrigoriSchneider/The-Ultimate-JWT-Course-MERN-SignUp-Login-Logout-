6686c756e59f937a8dc76c087f7d855bf81ddf66
unknown <grigorischneider@gmail.com>
2023-01-04 21:54:48 +0100

    Storing Token on Register & Login

--- Start/client/src/components/login/Login.js
+++ Start/client/src/components/login/Login.js
@@ -1,40 +1,53 @@
 import React, { useState } from "react";
+import http from "../../utils/http";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [userDetails, setUserDetails] = useState({
-        email: "",
-        password: "",
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  let navigate = useNavigate(); // Important
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prev) => {
+      return { ...prev, [name]: value };
     });
-    const [error, setError] = useState(null);
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUserDetails((prev) => {
-            return { ...prev, [name]: value };
-        });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // destructure data
+      const { data } = await http.post("/auth", userDetails);
+      localStorage.setItem("token", data);
+      navigate("/"); // redirect to homepage
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 400) {
+        setError(error.response.data);
+      }
+    }
+  };
+  return (
+    <form className="signup_form" onSubmit={handleSubmit}>
+      <label htmlFor="Email">Email</label>
+      <input type="email" name="email" onChange={handleChange} />
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(userDetails);
-    };
-    return (
-        <form className='signup_form' onSubmit={handleSubmit}>
-            <label htmlFor='Email'>Email</label>
-            <input type='email' name='email' onChange={handleChange} />
+      <label htmlFor="Password">Password</label>
+      <input type="password" name="password" onChange={handleChange} />
 
-            <label htmlFor='Password'>Password</label>
-            <input type='password' name='password' onChange={handleChange} />
+      {error && (
+        <div className="error_container">
+          <p className="form_error">{error}</p>
+        </div>
+      )}
 
-            {error && (
-                <div className='error_container'>
-                    <p className='form_error'>{error}</p>
-                </div>
-            )}
-
-            <button type='submit'>Login</button>
-        </form>
-    );
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
--- Start/client/src/components/register/Register.js
+++ Start/client/src/components/register/Register.js
@@ -1,44 +1,61 @@
 import React, { useState } from "react";
+import http from "../../utils/http";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const [userDetails, setUserDetails] = useState({
-        name: "",
-        email: "",
-        password: "",
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  let navigate = useNavigate(); // Important
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prev) => {
+      return { ...prev, [name]: value };
     });
-    const [error, setError] = useState(null);
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUserDetails((prev) => {
-            return { ...prev, [name]: value };
-        });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // destructure data
+      const { data } = await http.post("/user", userDetails);
+      localStorage.setItem("token", data);
+      navigate("/"); // redirect to homepage
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 400) {
+        setError(error.response.data);
+      }
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(userDetails);
-    };
-    return (
-        <form className='signup_form' onSubmit={handleSubmit}>
-            <label htmlFor='Name'>Name</label>
-            <input type='text' name='name' onChange={handleChange} />
+    // here you can see the object with the Token Status etc.
+    // const response = await http.post("/user", userDetails);
+    // console.log(response);
+  };
+  return (
+    <form className="signup_form" onSubmit={handleSubmit}>
+      <label htmlFor="Name">Name</label>
+      <input type="text" name="name" onChange={handleChange} />
 
-            <label htmlFor='Email'>Email</label>
-            <input type='email' name='email' onChange={handleChange} />
+      <label htmlFor="Email">Email</label>
+      <input type="email" name="email" onChange={handleChange} />
 
-            <label htmlFor='Password'>Password</label>
-            <input type='password' name='password' onChange={handleChange} />
+      <label htmlFor="Password">Password</label>
+      <input type="password" name="password" onChange={handleChange} />
 
-            {error && (
-                <div className='error_container'>
-                    <p className='form_error'>{error}</p>
-                </div>
-            )}
+      {error && (
+        <div className="error_container">
+          <p className="form_error">{error}</p>
+        </div>
+      )}
 
-            <button type='submit'>Register</button>
-        </form>
-    );
+      <button type="submit">Register</button>
+    </form>
+  );
 };
 
 export default Register;
