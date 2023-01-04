19f7b11c7c626fcf3eb781d0f590860d7d67fcc3
unknown <grigorischneider@gmail.com>
2023-01-04 15:22:47 +0100

    Verifying token on secured route

--- Start/server/routes/user.js
+++ Start/server/routes/user.js
@@ -2,24 +2,31 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+
+// User information
+router.get("/", auth, async (req, res) => {
+  const profile = await User.findById(req.user._id);
+  res.send(profile);
+});
 
 // Register User
 router.post("/", async (req, res) => {
-    const { email, name, password } = req.body;
+  const { email, name, password } = req.body;
 
-    // Checking User
-    let user = await User.findOne({ email });
-    if (user) return res.status(400).send("User already exists");
+  // Checking User
+  let user = await User.findOne({ email });
+  if (user) return res.status(400).send("User already exists");
 
-    // Save User Into Database
-    user = new User({ email, name, password });
-    await user.save();
+  // Save User Into Database
+  user = new User({ email, name, password });
+  await user.save();
 
-    // generate JWT token
-    const jwtData = { _id: user._id, name: user.name };
-    const token = jwt.sign(jwtData, process.env.JWTSECRET, { expiresIn: "2h" });
+  // generate JWT token
+  const jwtData = { _id: user._id, name: user.name };
+  const token = jwt.sign(jwtData, process.env.JWTSECRET, { expiresIn: "2h" });
 
-    res.send(token);
+  res.send(token);
 });
 
 module.exports = router;
