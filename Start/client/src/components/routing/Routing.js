709f6bb5d0784aa70ec997ed67e622565c8920cd
unknown <grigorischneider@gmail.com>
2023-01-05 15:07:22 +0100

    implementing logout feature

--- Start/client/src/components/routing/Routing.js
+++ Start/client/src/components/routing/Routing.js
@@ -4,15 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../login/Login";
 import Register from "../register/Register";
 import Home from "../home/Home";
+import Profile from "../profile/Profile";
+import Logout from "../logout/Logout";
 
 const Routing = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/logout" element={<Logout />} />
+    </Routes>
+  );
 };
 
 export default Routing;
