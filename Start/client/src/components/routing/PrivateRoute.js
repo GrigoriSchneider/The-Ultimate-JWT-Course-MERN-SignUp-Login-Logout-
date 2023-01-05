8ffa05fadb02af8c05a7334b51e1e09a7496642e
unknown <grigorischneider@gmail.com>
2023-01-05 16:37:12 +0100

    declared Navigate in privateRoute.js

--- Start/client/src/components/routing/PrivateRoute.js
+++ Start/client/src/components/routing/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = ({ user }) => {
   return user ? <Outlet /> : <Navigate to="login" />;
